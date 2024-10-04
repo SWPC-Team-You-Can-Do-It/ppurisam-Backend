a4487a2b93b90d6447376e682c2427db503a8767
jiwon <angry9908@gmail.com>
2024-10-04 23:25:46 +0900

    feat: 사용자 엔티티 관련 구현

---
+++ src/main/java/com/pprisam/backend/domain/user/repository/UserEntity.java
@@ -0,0 +1,45 @@
+package com.pprisam.backend.domain.user.repository;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue // 기본키 생성 전략인데 나중에 수정 필요할수도 default = auto
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private String status;
+
+    private LocalDateTime inactiveAt;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/pprisam/backend/domain/user/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.pprisam.backend.domain.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
