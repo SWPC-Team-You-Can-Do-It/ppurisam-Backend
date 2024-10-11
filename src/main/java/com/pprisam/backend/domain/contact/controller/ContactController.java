81e167e58737d498ac77b0f126affb57261ea953
bkw535 <bkw54570@gmail.com>
2024-10-11 16:59:02 +0900

    feat: contact id를 통한 상세조회 api 구현

--- src/main/java/com/pprisam/backend/domain/contact/controller/ContactController.java
+++ src/main/java/com/pprisam/backend/domain/contact/controller/ContactController.java
@@ -1,7 +1,8 @@
 package com.pprisam.backend.domain.contact.controller;
 
 import com.pprisam.backend.config.annotation.UserSession;
-import com.pprisam.backend.domain.contact.model.ContactDTO;
+import com.pprisam.backend.domain.contact.model.ContactRequest;
+import com.pprisam.backend.domain.contact.model.ContactResponse;
 import com.pprisam.backend.domain.contact.repository.ContactRepository;
 import com.pprisam.backend.domain.contact.service.ContactService;
 import com.pprisam.backend.domain.user.model.User;
@@ -22,20 +23,25 @@ public class ContactController {
     private ContactRepository contactRepository;
 
     @PostMapping("")
-    public ResponseEntity<ContactDTO> createContact(
-            @RequestBody ContactDTO contactDTO,
+    public ResponseEntity<ContactResponse> createContact(
+            @RequestBody ContactRequest contactDTO,
             @Parameter(hidden = true)
             @UserSession User user
     ){
 
         UserIdResponse userIdResponse = new UserIdResponse(user.getId());
-        ContactDTO savedContact = contactService.save(contactDTO, userIdResponse);
+        ContactResponse savedContact = contactService.save(contactDTO, userIdResponse);
         return ResponseEntity.ok(savedContact);
     }
 
     @GetMapping("")
-    public ResponseEntity<List<ContactDTO>> getAllContacts(){
-        List<ContactDTO> contacts = contactService.findAll(); // 전체 연락처 조회
+    public ResponseEntity<List<ContactResponse>> getAllContacts(){
+        List<ContactResponse> contacts = contactService.findAll(); // 전체 연락처 조회
         return ResponseEntity.ok(contacts);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContactResponse> getContactById(@PathVariable Long id){
+        return ResponseEntity.ok(contactService.findById(id));
     }
 }
