024d402db5492543223c5bb6fd285308fdb94c3f
jinsung <jinsung528@gmail.com>
2024-11-15 16:00:41 +0900

    refactor: 이미지 생성  프롬프팅 추가

---
+++ src/main/java/com/pprisam/backend/domain/ai/repository/ThemeEntity.java
@@ -0,0 +1,25 @@
+package com.pprisam.backend.domain.ai.repository;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "theme")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ThemeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String theme;
+
+    @Column(nullable = false)
+    private String description;
+}
